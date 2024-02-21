e30000a4b925fecb39ae6946e8bb2573e0a7067c
Axel <axel.pettersson.itg@gmail.com>
2024-02-21 15:24:55 +0100

    Move to devops

---
+++ App/Models/Booking.cs
@@ -0,0 +1,8 @@
+﻿namespace ParkingApp.Models
+{
+	public class Booking
+	{
+		public required int Id { get; set; }	
+		public required DateTime Date { get; set; }	
+	}
+}
---
+++ App/Models/ParkingAppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ParkingApp.Models
+{
+	public class ParkingAppDbContext : DbContext
+	{
+		public ParkingAppDbContext(DbContextOptions<ParkingAppDbContext> options) : base(options) 
+		{
+			
+		}	
+
+		public DbSet<Booking> Bookings { get; set; }	
+	}
+}
